f4a62a090911e22fcfb03f67c27544d73ddddbc4
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-11-09 14:23:20 +0500

    Making of main file with the help of OOP HW files

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+//Using the files provided in OOP HW to recreate main file
+int main(int argc, char * argcv[]){
+    Game game;  //We will create a class game that will be like a backbone to our game that will import images, create window,etc
+    srand(time(NULL));
+    if (!game.init()){
+        printf("Failed to initialize!\n")
+        return 0;
+    }
+
+    if (!game.loadMedia()){
+        printf( "Failed to load media!\n" );
+        return 0;
+    }
+    
+    game.run();
+    game.close();
+
+    return 0;
+}
