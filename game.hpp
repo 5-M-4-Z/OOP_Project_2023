abf7df7f0b96f5487b873933176d28ac1a700fdf
HussainUmer <hf08040@st.habib.edu.pk>
2023-11-21 13:14:45 +0500

    Background Change+Audio

--- game.hpp
+++ game.hpp
@@ -40,7 +40,7 @@ class Game{
         SDL_Texture* assets = NULL;
 
         //Music that we fill figure out for later;
-        // Mix_Music *bg_music = NULL;
+        Mix_Music *bg_music = NULL;
 
 
     public:
