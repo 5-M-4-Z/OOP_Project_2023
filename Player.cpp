18fee94f4ef9ae6fda3d32d3cc7b9ccc28032b8a
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-11-27 16:43:07 +0500

    Completed player class functions added some modificatio for bullets

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,7 @@
 
 Player::Player(int x, int y){
     src = {111,44,149,182};    mover = {x, y, 75, 75};
+    delay = 0;
 
 }
 
@@ -33,9 +34,37 @@ void Player::move(int width, int height){
 }
 
 void Player::shoot(){
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_F]){
+        f_pressed = 1;
+    }
 
+    std::cout << delay << '\n';
+    if (f_pressed && delay == 0){
+        Bullet bullet(mover.x, mover.y);
+        bullets_array.push_back(bullet);
+        delay = -5;
+        f_pressed = 0;
+    }
+    else if (f_pressed) {delay++;}
 }
 
 void Player::display(SDL_Renderer* renderer, SDL_Texture* assets){
     SDL_RenderCopy(renderer, assets, &src, &mover);
 }
+
+void Player::display_bullet(SDL_Renderer* renderer, SDL_Texture* assets){
+    for (int i=0; i<bullets_array.size(); i++){
+        bullets_array[i].display(renderer, assets);
+    }
+}
+
+void Player::move_bullet(){
+    for (int i=0; i<bullets_array.size(); i++){
+        bullets_array[i].move_bullet();
+    }
+}
+
+void Player::set_delay(int x){delay = x;}
+
+int Player::get_f_pressed(){return f_pressed;}
--- Player.hpp
+++ Player.hpp
@@ -1,11 +1,16 @@
 #include <SDL.h>
-
+#include <vector>
+#include <iostream>
+#include "Bullet.hpp"
 #pragma once
 
 class Player{
     private:
         SDL_Rect src, mover;
         int health=100, exp=0, power=20;
+        std::vector<Bullet> bullets_array;
+        int delay;
+        bool f_pressed;
     public:
         Player(int x, int y);
         void move(int width, int height);
@@ -13,4 +18,8 @@ class Player{
         void gainExp();
         void display(SDL_Renderer* renderer, SDL_Texture* assets);
         void gainPow();
+        void display_bullet(SDL_Renderer* renderer, SDL_Texture* assets);
+        void move_bullet();
+        void set_delay(int x);
+        int get_f_pressed();
 };
