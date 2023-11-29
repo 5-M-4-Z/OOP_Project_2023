d18043c4eab6c818c2a4aec837d44f2b75755990
HussainUmer <hf08040@st.habib.edu.pk>
2023-11-30 01:31:21 +0500

    Added 1 more Enemy Plane

--- ObjectCreator.cpp
+++ ObjectCreator.cpp
@@ -1,14 +1,17 @@
 #include "ObjectCreator.hpp"
 
 Object* ObjectCreator::getObject(int x, int y){
-    int random = rand()%3;
+    int random = rand()%4;
     if(random == 0){                            // random object.
         return new EnemyPlane1(x,y);
     }                                            
     else if(random == 1){
         return new EnemyPlane2(x,y);
     }
-    else{
+    else if (random == 2){
         return new EnemyPlane3(x,y);
+    }
+    else{
+        return new EnemyPlane4(x,y);
     }
 }
--- ObjectCreator.hpp
+++ ObjectCreator.hpp
@@ -2,7 +2,7 @@
 #include "EnemyPlane1.hpp"
 #include "EnemyPlane2.hpp"
 #include "EnemyPlane3.hpp"
-
+#include "EnemyPlane4.hpp"
 class ObjectCreator{
 public:
     Object* getObject(int,int);
