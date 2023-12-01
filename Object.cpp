ea3c028c0dd1d0119d02a6362ded7e41bf40e16d
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-12-02 00:46:44 +0500

    added some functions

--- Object.cpp
+++ Object.cpp
@@ -5,4 +5,13 @@ void Object::draw(SDL_Renderer* renderer, SDL_Texture* assets){              // 
     SDL_RenderCopyEx(renderer, assets, &src, &mover, 0, NULL, SDL_FLIP_VERTICAL); 
 }
 void Object::move(){};
+
 void Object::display_bullet(SDL_Renderer* renderer, SDL_Texture* assets){};
+
+SDL_Rect Object::get_mover(){
+    return mover;
+}
+
+void Object::collision(SDL_Rect player_mover){}
+
+void Object::collision_player_bullet(Player player){}
--- Object.hpp
+++ Object.hpp
@@ -1,3 +1,4 @@
+#include "Player.hpp"
 #include<SDL.h>
 #pragma once
 
@@ -9,5 +10,8 @@ public:
     void draw(SDL_Renderer* renderer, SDL_Texture* assets);
     virtual void move();  
     virtual void display_bullet(SDL_Renderer* renderer, SDL_Texture* assets);
+    SDL_Rect get_mover();
     // virtual void shoot();  
+    virtual void collision(SDL_Rect player_mover);
+    virtual void collision_player_bullet(Player player);
 };
