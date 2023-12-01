d7194b946c7905a11b1e174b0abf836ac5ec198a
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-12-01 21:43:38 +0500

    Modified so it incorporates the deleted files functionality

--- TopGun.cpp
+++ TopGun.cpp
@@ -13,8 +13,12 @@ void TopGun::drawObjects(SDL_Renderer* renderer, SDL_Texture* assets)
 // creates new objects 
 void TopGun::createObject(int x, int y){
     // std::cout << "Mouse clicked at: " << x << " -- " << y << std::endl;
-    ObjectCreator obj;                             // creating an object of the class ObjectCreator.
-    Object* o = obj.getObject(x,y);                  // creating a pointer to the object of the class Unit.
+    // ObjectCreator obj;                             // creating an object of the class ObjectCreator.
+    // Object* o = obj.getObject(x,y);                  // creating a pointer to the object of the class Unit.
+
+    // using same logic but only for enemyPlane1
+    int random  = rand() % 4;
+    Object* o = new EnemyPlane1(x,y,random);
     lst.push_back(o);                              
 }
 
@@ -25,3 +29,9 @@ void TopGun::createObject(int x, int y){
 //     lst.clear(); 
 //     cout<<"List Cleared"<<endl;            //clears all the items from list             
 // }
+
+void TopGun::display_bullet(SDL_Renderer* renderer, SDL_Texture* assets){
+    for (Object* enemy : lst){
+        enemy->display_bullet(renderer, assets);
+    }
+}
--- TopGun.hpp
+++ TopGun.hpp
@@ -1,8 +1,5 @@
 #include<SDL.h>
 #include "EnemyPlane1.hpp"
-#include "EnemyPlane2.hpp"
-#include "ENemyPlane3.hpp"
-#include "ObjectCreator.hpp"
 #include<vector>
 #include<list>
 using namespace std;
@@ -16,4 +13,5 @@ public:
     void createObject(int, int);
     // create destructor which deletes all dynamic objects
     // ~TopGun();
+    void display_bullet(SDL_Renderer* renderer, SDL_Texture* assets);
 };
