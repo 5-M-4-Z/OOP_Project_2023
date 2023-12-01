9aa2d9c143f7ddcd6c5effa35299610771715f01
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-12-01 21:41:03 +0500

    These files objective can be done without these files

--- ObjectCreator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "EnemyPlane1.hpp"
-#include "EnemyPlane2.hpp"
-#include "EnemyPlane3.hpp"
-#include "EnemyPlane4.hpp"
-class ObjectCreator{
-public:
-    Object* getObject(int,int);
-};
