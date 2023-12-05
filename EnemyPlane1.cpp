9617097502c04ad504211c03d4e583b4d3d88c18
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-12-05 13:05:48 +0500

    removed collision bullet remove after end of screen added health thingy

--- EnemyPlane1.cpp
+++ EnemyPlane1.cpp
@@ -7,6 +7,7 @@ EnemyPlane1::EnemyPlane1(){
 
     // it will display plane on x = 250, y = 50 location, the size of plane is 75 width, 75 height
     mover = {250, 50, 75, 75};
+    name = "enemy";
 }
 EnemyPlane1::EnemyPlane1(int x, int y, int random)
 {
@@ -29,6 +30,10 @@ EnemyPlane1::EnemyPlane1(int x, int y, int random)
     shootTimer = 0;
 }
 
+void EnemyPlane1::display(SDL_Renderer* renderer, SDL_Texture* assets){
+    SDL_RenderCopyEx(renderer, assets, &src, &mover, 0, NULL, SDL_FLIP_VERTICAL);
+}
+
 void EnemyPlane1::move(){
     mover.y += 3;
 }
@@ -47,31 +52,22 @@ void EnemyPlane1::shoot(){
 void EnemyPlane1::display_bullet(SDL_Renderer* renderer, SDL_Texture* assets){
     this->shoot();
     //This function needs to be overloaded specifically for thhe enemy so that bullet is faster than the enemy plane speed
-    this->move_bullet();
+    // this->move_bullet();
     for (int i=0; i<bullets_array.size(); i++){
         bullets_array[i].display(renderer, assets, 1); // passing dummy 1 to flip the bullet for the enemy
     }
 }
 
-void EnemyPlane1::move_bullet(){
+void EnemyPlane1::move_bullet(int height){
+    int x;
     for (int i=0; i<bullets_array.size(); i++){
-        bullets_array[i].move_bullet(0);
-    }
-}
-
-void EnemyPlane1::collision(SDL_Rect enmy_mover){
-    for (auto bullet : bullets_array){
-        SDL_Rect bullet_mover = bullet.get_mover();
+       bullets_array[i].move_bullet(0);
+        int h = (bullets_array[i].get_mover()).y;
         
-        if (SDL_HasIntersection(&bullet_mover, &enmy_mover)){
-            std::cout << "enemy bullet to player\n";
+        if (h > height){
+            bullets_array.erase(bullets_array.begin() + i);
+            std::cout << "bullet destroyed\n";
+            x += 1;
         }
-    }
-}
-
-void EnemyPlane1::collision_player_bullet(Player player){
-
-    for(auto bullet : bullets_array){
-        player.collision_enemy_bullet(bullet);
     }
 }
--- EnemyPlane1.hpp
+++ EnemyPlane1.hpp
@@ -3,23 +3,22 @@
 #include <vector>
 #include <iostream>
 #include "Bullet.hpp"
+#include "Health.hpp"
 #pragma once
 
 class EnemyPlane1 : public Object
 {
 private:
-    std::vector<Bullet> bullets_array;
     int delay;
     int shootTimer;
 public:
     void move();
     void shoot();
+    void display(SDL_Renderer* renderer, SDL_Texture* texture);
     void display_bullet(SDL_Renderer* renderer, SDL_Texture* assets);
-    void move_bullet();
-    void set_delay(int x);
+    void move_bullet(int height);
+    // void set_delay(int x);
     EnemyPlane1(int x, int y, int random);
     EnemyPlane1();
     // ~EnemyPlane1();
-    void collision(SDL_Rect player_mover);
-    void collision_player_bullet(Player player);
 };
