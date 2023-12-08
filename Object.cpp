077c7e8572bd19e9e7f7bee46b738c95dea2e8ae
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-12-08 14:48:45 +0500

    commented a single line

--- Object.cpp
+++ Object.cpp
@@ -23,7 +23,7 @@ bool Object::collision_current_opponent_bullet(Object* opponent){
         if (SDL_HasIntersection(&opponent_mover, &bullet_mvr)){
             // bullet.explode(renderer, assets);
             bullets_array.erase(bullets_array.begin() + i);
-            std::cout << this->get_power() << '\n';
+            
             opponent->reduce_health(this->get_power());
             return 1;
         }
