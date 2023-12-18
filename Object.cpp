b6343f94ed8722f304a60586e3693f12f7275413
5-M-4-Z <sz08464@st.habib.edu.pk>
2023-12-19 03:57:40 +0500

    This file was mixture of Aircraft class and object so now it is separated

--- Object.cpp
+++ Object.cpp
@@ -1,67 +1,19 @@
 #include "Object.hpp"
 #include <iostream>
 
-void Object::display(SDL_Renderer* renderer, SDL_Texture* assets){}
-void Object::move(){};
-
+void Object::display(SDL_Renderer* renderer, SDL_Texture* assets){
+    
+    if (name == "Enemy"){
+        // SDL_RenderCopy(renderer, assets, &src, &mover);
+        SDL_RenderCopyEx(renderer, assets, &src, &mover, 0, NULL, SDL_FLIP_VERTICAL);
+    }
+    else {
+        SDL_RenderCopy(renderer, assets, &src, &mover);
+    }
+}
+// void Object::move(){};
 void Object::display_bullet(SDL_Renderer* renderer, SDL_Texture* assets){};
 
 SDL_Rect Object::get_mover(){
     return mover;
 }
-
-void Object::move(int x, int y){}
-void Object::shoot(){}
-void Object::move_bullet(){}
-void Object::move_bullet(int height){}
-void Object::set_delay(int x){}
-
-bool Object::collision_current_opponent_bullet(Object* opponent){
-    for (int i=0; i<bullets_array.size(); i++){
-        Bullet bullet = bullets_array[i];
-        SDL_Rect bullet_mvr = bullet.get_mover(), opponent_mover = opponent->get_mover();
-        if (SDL_HasIntersection(&opponent_mover, &bullet_mvr)){
-            // bullet.explode(renderer, assets);
-            bullets_array.erase(bullets_array.begin() + i);
-            
-            opponent->reduce_health(this->get_power());
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void Object::collision_player_enemy_bullet(Object* opponent){
-    int size = opponent->bullets_array.size();
-    for (int i=0; i<bullets_array.size(); i++){
-        SDL_Rect p_bullet_mvr = (bullets_array[i]).get_mover();
-
-        for (int j=0; j<size; j++){
-            SDL_Rect e_bullet_mvr = (opponent->bullets_array[j]).get_mover();
-
-            if (SDL_HasIntersection(&p_bullet_mvr, &e_bullet_mvr)){
-                std::cout << "Bullet to bullet collision\n";
-                bullets_array.erase(bullets_array.begin() + i);
-                opponent->delete_bullet(i);
-            }
-        }
-    }
-}
-
-void Object::add(){}
-
-void Object::explode(SDL_Renderer* renderer, SDL_Texture* assets){}
-
-void Object::reduce_health(int power){}
-
-bool Object::get_destroyed(){return destroyed;}
-
-void Object::delete_bullet(int i){
-    this->bullets_array.erase(bullets_array.begin() + i);
-}
-
-int Object::get_power(){};
-
-int Object::get_health(){};
-
-bool Object::is_completely_destroyed(){}
--- Object.hpp
+++ Object.hpp
@@ -1,40 +1,17 @@
 // #include "Player.hpp"
 #include<SDL.h>
 #include <iostream>
-#include "Bullet.hpp"
+// #include "Bullet.hpp"
 #include <vector>
 #pragma once
 
 
 class Object{
-protected:    // Protected bcz we want to access these variables in the derived classes.
-    SDL_Rect src, mover;
-    bool destroyed=0;
-    std::vector<Bullet> bullets_array;
-public:
-    virtual void display(SDL_Renderer* renderer, SDL_Texture* assets);
-    virtual void move();
-    virtual void display_bullet(SDL_Renderer* renderer, SDL_Texture* assets);
-    SDL_Rect get_mover();
-    // virtual void shoot();  
-    virtual void move(int x, int y);
-    virtual void shoot();
-    virtual void move_bullet();
-    virtual void move_bullet(int height);
-    
-    virtual void set_delay(int x);
-
-    bool collision_current_opponent_bullet(Object* opponent);
-    void collision_player_enemy_bullet(Object* opponent);
-
-    virtual void add();
-    virtual void explode(SDL_Renderer* renderer, SDL_Texture* assets);
-
-    bool get_destroyed();
-
-    void delete_bullet(int i);
-    virtual void reduce_health(int power);
-    virtual int get_power();
-    virtual int get_health();
-    virtual bool is_completely_destroyed();
+    protected:    // Protected bcz we want to access these variables in the derived classes.
+        std::string name;
+        SDL_Rect src, mover;
+    public:
+        virtual void display(SDL_Renderer* renderer, SDL_Texture* assets);
+        virtual void display_bullet(SDL_Renderer* renderer, SDL_Texture* assets);
+        SDL_Rect get_mover();
 };
